d08a42bdd0f2998c9db1fb03a7869a34489e4e4a
wonjun <red0523@naver.com>
2017-07-20 13:42:45 +0900

    1차dao완료

--- src/dao/SyllabusDAO.java
+++ src/dao/SyllabusDAO.java
@@ -137,12 +137,10 @@ public class SyllabusDAO {
 		MyLectureVO lecture = null;
 		try {
 			conn = getConnection();
-<<<<<<< HEAD
+
 			pstmt = conn.prepareStatement(
 					"select l.lec_no,l.lec_name,l.lec_total_date,l.enroll_num,s.lec_time from lecture l, syllabus s where l.lec_no = s.lec_no and l.teacher_id = ?");
-=======
-			pstmt = conn.prepareStatement("select l.lec_no,l.lec_name,l.lec_total_date,l.enroll_num,s.lec_time from lecture l, syllabus s where l.lec_no = s.lec_no and l.teacher_id = ?");
->>>>>>> dd28db3f413746914fab08e7390bbfec6f580b7a
+
 			pstmt.setString(1, id);
 			rs = pstmt.executeQuery();
 			while (rs.next()) {
